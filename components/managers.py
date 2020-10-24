a4838dc340d1688580aa9e93bd2003c11d7a62ff
Sang Bui <xsang.bui@gmail.com>
2020-10-24 21:37:43 +0700

    Managers: add post method to insert bulk records

--- components/managers.py
+++ components/managers.py
@@ -21,6 +21,29 @@ class Managers(Resource):
             'result': managers_schema.dump(managers)
         }
 
+    def post(self):
+        default_password = 'p@ssw0rd'
+        try:
+            data = request.get_json()
+            jwt = utils.get_jwt()
+            role = jwt['role']
+        except Exception:
+            return utils.return_auth_err()
+        required_roles = ['manager', 'admin']
+        if role not in required_roles:
+            return utils.return_unauthorized()
+        managers_to_add = []
+        for manager in data:
+            try_manager = db.Manager.query \
+                .filter(db.Manager.username == manager['username']) \
+                .one_or_none()
+            if try_manager is not None:
+                continue
+            managers_to_add.append(db.Manager(manager['name'], manager['email'], manager['username'], default_password, 'manager'))
+        return {
+            'success': utils.db_insert(managers_to_add)
+        }
+
 class Manager(Resource):
     def get(self, manager_id):
         try:
